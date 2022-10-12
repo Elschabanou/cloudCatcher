46cf7414a089b1e10992ff650407cc600bac03ce
moemchen13 <85173797+moemchen13@users.noreply.github.com>
2022-10-12 13:40:05 +0200

    response 401

--- src/weatherData/main.py
+++ src/weatherData/main.py
@@ -1,3 +1,4 @@
+import requests
 from geopy.geocoders import Nominatim
 
 
@@ -9,11 +10,17 @@ def get_weather(city: str) -> str:
         raise Exception("City not found")
     # test
     latitude = getLoc.latitude
+    latitude = 33.44
     longitude = getLoc.longitude
+    longitude = -94.04
     api_key = "65e18d8ec814590c386dffb416021cc6S"
-    request_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&exclude=minutely,hourly,daily,alerts&appid={api_key}"
+    request_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&exclude=minutely,hourly&appid={api_key}"
+    response = requests.get(request_url)
+    print(response)
     
-    kind_of_weather = 
-    temperature = 
-    probability_of_rain = 
-    message = f"Hey ure weather in city will be {kind_of_weather}, with a temperature of {temperature} degree and a probability {probability_of_rain} of rain "
+    #kind_of_weather = 
+    #temperature = 
+    #probability_of_rain = 
+    #message = f"Hey ure weather in city will be {kind_of_weather}, with a temperature of {temperature} degree and a probability {probability_of_rain} of rain "
+
+get_weather("Stuttgart")
