129f64f4cf59b20bc396bf5557fe96ed7ce66ed9
moemchen13 <85173797+moemchen13@users.noreply.github.com>
2022-10-12 13:31:18 +0200

    Added template output

--- src/weatherData/main.py
+++ src/weatherData/main.py
@@ -12,3 +12,8 @@ def get_weather(city: str) -> str:
     longitude = getLoc.longitude
     api_key = "65e18d8ec814590c386dffb416021cc6S"
     request_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&exclude=minutely,hourly,daily,alerts&appid={api_key}"
+    
+    kind_of_weather = 
+    temperature = 
+    probability_of_rain = 
+    message = f"Hey ure weather in city will be {kind_of_weather}, with a temperature of {temperature} degree and a probability {probability_of_rain} of rain "
