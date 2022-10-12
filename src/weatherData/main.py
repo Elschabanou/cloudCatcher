f9acf93b10ac7075beab40c0e4c21cd4469f7407
unknown <felix@schabana.de>
2022-10-12 13:23:06 +0200

    added api-key to main

--- src/weatherData/main.py
+++ src/weatherData/main.py
@@ -1,4 +1,3 @@
-
 from geopy.geocoders import Nominatim
 
 
@@ -8,8 +7,8 @@ def get_weather(city: str) -> str:
         getLoc = loc.geocode(city)
     except:
         raise Exception("City not found")
-        
+
     latitude = getLoc.latitude
     longitude = getLoc.longitude
-    api_key = 1
-    request_url=f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&exclude=minutely,hourly,daily,alerts&appid={api_key}"
+    api_key = "65e18d8ec814590c386dffb416021cc6S"
+    request_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&exclude=minutely,hourly,daily,alerts&appid={api_key}"
